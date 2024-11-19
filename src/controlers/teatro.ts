277e8c0607dd37ca25df934628907e64452e8832
Miranda Callejón <mirandacallejonh@gmail.com>
2024-11-19 13:03:17 +0100

    función petición datos de las butacas por rangos ok

--- src/controlers/teatro.ts
+++ src/controlers/teatro.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import Theatre from "../models/teatro";
-import { Sequelize } from "sequelize";
+import { Sequelize, QueryTypes } from "sequelize";
+import sequelize from "../db/connections";
+
 
 export const getTheatres = async (req: Request, res: Response) => {
     const theatreList = await Theatre.findAll();
@@ -91,4 +93,19 @@ export const getAccessibility = async (req: Request, res: Response) => {
       });
 
     res.json(accessibilityList);
+};
+
+
+export const getSeatCount = async (req: Request, res: Response) => {
+    const counts = await sequelize.query(`
+        SELECT 
+          SUM(CASE WHEN seat_count < 100 THEN 1 ELSE 0 END) AS less_than_100,
+          SUM(CASE WHEN seat_count BETWEEN 100 AND 299 THEN 1 ELSE 0 END) AS between_100_and_300,
+          SUM(CASE WHEN seat_count BETWEEN 300 AND 499 THEN 1 ELSE 0 END) AS between_300_and_500,
+          SUM(CASE WHEN seat_count BETWEEN 500 AND 999 THEN 1 ELSE 0 END) AS between_500_and_999,
+          SUM(CASE WHEN seat_count >= 1000 THEN 1 ELSE 0 END) AS greater_than_1000
+        FROM salas;
+      `, { type: QueryTypes.SELECT });
+      res.json(counts);
+
 };
--- src/routes/teatro.ts
+++ src/routes/teatro.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import { getTheatres, getTheatre, deleteTheatre, postTheatre, updateTheatre, getAccessibility} from '../controlers/teatro'
+import { getTheatres, getTheatre, deleteTheatre, postTheatre, updateTheatre, getAccessibility, getSeatCount} from '../controlers/teatro'
 
 const router = Router();
 
 router.get('/', getTheatres)
 router.get('/accessibility', getAccessibility)
+router.get('/seat-count', getSeatCount)
 router.get('/:id', getTheatre)
 router.delete('/:id', deleteTheatre)
 router.post('/', postTheatre)
